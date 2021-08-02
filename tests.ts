5e0d9050a48d7794ae9d6765c3ba6620c7a04a01
MakeAndLearn <58392278+MakeAndLearn@users.noreply.github.com>
2021-08-02 09:52:42 +0200

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -4,13 +4,13 @@ input.onGesture(Gesture.Shake, function () {
     microshield.Servo(microshield.Servos.S7, 90)
 })
 input.onButtonPressed(Button.A, function () {
-    microshield.StepperDegree(microshield.Steppers.STEP1, 45)
-    microshield.StepperDegree(microshield.Steppers.STEP2, -45)
+    microshield.Stepper(microshield.Steppers.STEP1, microshield.stepUnit.Degrees,45)
+    microshield.Stepper(microshield.Steppers.STEP2, microshield.stepUnit.Degrees, -45)
     microshield.MotorRun(microshield.Motors.M4, 100)
 })
 input.onButtonPressed(Button.B, function () {
-    microshield.StepperDegree(microshield.Steppers.STEP1, -90)
-    microshield.StepperDegree(microshield.Steppers.STEP2, 90)
+    microshield.Stepper(microshield.Steppers.STEP1, microshield.stepUnit.Degrees, 90)
+    microshield.Stepper(microshield.Steppers.STEP2, microshield.stepUnit.Degrees, -90)
     microshield.MotorRun(microshield.Motors.M4, -100)
 })
 microshield.MotorStop(microshield.Motors.M4)
